bee06f87fa17ba0be9d68fc84d9aeea01aaeb4c7
Joseph Loic Bisso-Bi-Ela <j.bisso-bi-ela@tu-braunschweig.de>
2020-11-10 17:45:28 +0100

    Calculator (plus und minus). Lauffaehig

---
+++ blatt1/Calculator.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Calculator{
+	public static void main (String[] args){
+		double zahl1 = 0,
+		       zahl2 = 0,
+			   zahl3 = 0;
+			   
+		System.out.println("Geben Sie 2 Zahlen ein:\n");
+		System.out.println("Erste Zahl:  ");
+		Scanner scz1 = new Scanner(System.in);
+		zahl1 = scz1.nextInt();
+		System.out.println("\nZweite Zahl:  ");
+		Scanner scz2 = new Scanner(System.in);
+		zahl2 = scz2.nextInt();
+		
+        System.out.println("\nWelche Rechenoperation soll ausgeführt werden?\n Addition (+): 1 , Subtraktion (-): 2 ;");
+		Scanner sc = new Scanner(System.in);
+		int Operator = sc.nextInt();
+
+        switch (Operator){
+			case 1 : { 
+			System.out.println("Addition wird ausgefüht");
+			zahl3 = zahl1 + zahl2; break;
+			}
+			case 2 : { 
+			System.out.println("Subtraktion wird ausgefüht");
+			zahl3 = zahl1 - zahl2; break;
+			}
+		}
+		
+		System.out.println("Das Ergebnis ist: "+ zahl3 );
+	}
+}
