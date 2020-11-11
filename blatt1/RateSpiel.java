6306c9cf6dd409ee8866ad44895e6dce16d0951e
Joseph Loic Bisso-Bi-Ela <j.bisso-bi-ela@tu-braunschweig.de>
2020-11-11 23:55:21 +0100

    RateSpiel funktionsfaehig. Problem: Aufgabenstellung nicht richtig verstanden

--- blatt1/RateSpiel.java
+++ blatt1/RateSpiel.java
@@ -1,5 +1,50 @@
-public class RateSpiel{
-	public static void main(String[] args){
-		System.out.println("Ready");
+import java.util.Scanner;
+
+public class RateSpiel{	
+	public static void main(String[] args){		
+		
+		int vZahl = 0,
+		    arg = args.length,
+			aVersuch = -6969; //Nice
+		
+		if (arg==0){
+			System.out.println(" Sie haben vergessen ein Zahl zu geben!\nGeben Sie jetzt bitte eine Zahl ein: ");
+			
+			Scanner sc = new Scanner(System.in);
+			vZahl = sc.nextInt();
+		}
+		else{
+			vZahl = Integer.parseInt(args[0]);
+		}
+		
+		aVersuch = computerRaten(vZahl);
+		
+		System.out.println("\nEs wurden "+ aVersuch + " Versuche benötigt, um die Zahl zu raten");	
+			
+	}
+
+	
+    static int computerRaten(int vZahl){
+		
+	    double dZahl = 100 * Math.random();
+	    int zZahl = (int) dZahl,
+    	    c = 0,
+	    	uGrenze = 0,
+		    oGrenze = 100;
+			
+		    while (vZahl != zZahl){
+			    c++;
+			
+			    if (vZahl<zZahl){
+				    oGrenze = zZahl;
+				    zZahl = (zZahl + uGrenze) / 2;
+			    }  
+			    if (vZahl>zZahl){
+				    uGrenze = zZahl;
+				    zZahl = (zZahl + oGrenze) / 2;
+			    }
+		    }
+			
+		return c;
 	}
 }
