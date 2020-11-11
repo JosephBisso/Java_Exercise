cba631fa9128322bb4dbf2777da1d7943fd65676
Joseph Loic Bisso-Bi-Ela <j.bisso-bi-ela@tu-braunschweig.de>
2020-11-12 00:04:27 +0100

    RateSpiel funktionsfaehig. Konplett

--- blatt1/RateSpiel.java
+++ blatt1/RateSpiel.java
@@ -8,7 +8,7 @@ public class RateSpiel{
 			aVersuch = -6969; //Nice
 		
 		if (arg==0){
-			System.out.println(" Sie haben vergessen ein Zahl zu geben!\nGeben Sie jetzt bitte eine Zahl ein: ");
+			System.out.println("Sie haben vergessen eine Zahl zu uebergeben!\nGeben Sie jetzt bitte eine Zahl ein: ");
 			
 			Scanner sc = new Scanner(System.in);
 			vZahl = sc.nextInt();
@@ -19,8 +19,12 @@ public class RateSpiel{
 		
 		aVersuch = computerRaten(vZahl);
 		
-		System.out.println("\nEs wurden "+ aVersuch + " Versuche benötigt, um die Zahl zu raten");	
-			
+		if (aVersuch == 1){
+			System.out.println("\nEs wurde ein einziger Versuch benoetigt, um die Zahl zu raten. Du könntest heute auch im Lotto gewinnen!!");	
+		}
+		else {
+			System.out.println("\nEs wurden "+ aVersuch + " Versuche benoetigt, um die Zahl zu raten");	
+		}	
 	}
 
 	
@@ -32,18 +36,17 @@ public class RateSpiel{
 	    	uGrenze = 0,
 		    oGrenze = 100;
 			
-		    while (vZahl != zZahl){
-			    c++;
-			
-			    if (vZahl<zZahl){
-				    oGrenze = zZahl;
-				    zZahl = (zZahl + uGrenze) / 2;
-			    }  
-			    if (vZahl>zZahl){
-				    uGrenze = zZahl;
-				    zZahl = (zZahl + oGrenze) / 2;
-			    }
-		    }
+			if (vZahl == zZahl){
+				return 1;
+			}
+			else{	
+		        while (vZahl != zZahl){
+			        c++;
+			  
+			    double dZahl2 = 100 * Math.random();
+	            zZahl = (int) dZahl2;			
+		        }
+			}
 			
 		return c;
 	}
