495932a148bdd2b65a74e2d6c42cb43050e6a766
y0090855 <mohariri95@hotmail.com>
2020-11-13 21:28:45 +0100

    RateSpiel Selber Raten. laeuft einwandfrei

--- blatt1/RateSpiel.java
+++ blatt1/RateSpiel.java
@@ -1,5 +1,45 @@
+import java.util.Scanner;
+
 public class RateSpiel{
 	public static void main(String[] args){
-		System.out.println("Ready");
+		int arg = args.length,
+		    gZahl = 0,
+		    zRZahl = selberRaten();
+		
+		if (arg == 0){				
+		 	System.out.println("\nEs wird jetzt eine Zufallszahl generiert. Versuchen Sie diese Zahl zu raten!\nGeben dafür eine Zahl ein:");
+			
+			Scanner sc = new Scanner (System.in);
+			gZahl = sc.nextInt();
+			
+			while(gZahl!= zRZahl){
+				if (gZahl < zRZahl){
+					System.out.println("\nPech gehabt!\nDie zu ratende Zahl ist größer als die von Ihnen eingegebene Zahl. \nGeben Sie eine groessere Zahl ein:");
+				}
+				else{
+					System.out.println("\nPech gehabt!\nDie zu ratende Zahl ist kleiner als die von Ihnen eingegebene Zahl. \nGeben Sie eine kleinere Zahl ein:");
+				}
+
+				Scanner sc2 = new Scanner (System.in);
+			    gZahl = sc2.nextInt();
+
+			}
+			
+			if (gZahl == zRZahl){
+				System.out.println("\nBravo!! Sie haben die Zahl richtig erraten. Sie bekommen innerhalb von 2 Woche eine Belohnung von 1 Cent auf Ihren Konto überwiesen.");
+			}
+			
+		}
+		
+		
+		
+        
+	}
+	
+	static int selberRaten (){
+		double zahl = 100 * Math.random();
+		int zZahl = (int)zahl;
+		
+		return zZahl; 
 	}
 }
