669120e14f9a6687b118194676389c3c4d601009
Joseph Loic Bisso-Bi-Ela <j.bisso-bi-ela@tu-braunschweig.de>
2020-11-17 21:27:03 +0100

    FizzBuzz erste Version. Lauffaehig

---
+++ blatt2/FizzBuzz.java
@@ -0,0 +1,37 @@
+public class FizzBuzz{
+	public static void main(String[] args){
+		
+		int n = Integer.parseInt(args[0]);
+		
+		int[] aZahl = new int [n];
+		String[] sZahl = new String[n];
+		
+		String auflistung = "";
+		
+		for (int i=0; i<n; i++){
+			
+			aZahl[i] = i+1 ;
+			sZahl[i] = Integer.toString(aZahl[i]);
+			
+			if (aZahl[i]%3==0 & !(aZahl[i]%5==0)){
+				sZahl[i] = "Fizz";
+			}
+			else if (!(aZahl[i]%3==0) & aZahl[i]%5==0){
+				sZahl[i] = "Buzz";
+			}
+			else if (aZahl[i]%3==0 & aZahl[i]%5==0){
+				sZahl[i] = "Fizz-Buzz";
+			}
+			
+			if (i!=n-1){
+			    auflistung += (sZahl[i] + ",");
+			}
+			else{
+				auflistung += sZahl[i];
+			}
+		}
+		
+		System.out.println(auflistung);
+		
+	} 
+}
