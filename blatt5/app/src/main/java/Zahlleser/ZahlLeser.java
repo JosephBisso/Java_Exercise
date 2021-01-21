abc790e8a2c49a5dd54702df5bb3e9d11f130a43
Joseph Loic Bisso-Bi-Ela <j.bisso-bi-ela@tu-braunschweig.de>
2021-01-21 15:35:27 +0100

    Endlich Fertig 2.0

---
+++ blatt5/app/src/main/java/Zahlleser/ZahlLeser.java
@@ -0,0 +1,61 @@
+package Zahlleser;
+
+import java.util.*;
+
+public class ZahlLeser {
+	
+	public static void main (String[] args) {
+		
+		int zahl = 0;
+		try {
+			
+			Scanner sc = new Scanner(args[0]);
+			zahl = sc.nextInt();
+			
+			istKleinerAlsZehn(zahl);
+			
+			System.out.println("\nSie haben die Zahl " + zahl + " eingegeben");
+		
+		} catch (ArrayIndexOutOfBoundsException abe) {
+			
+			zahl = 3;
+			System.out.println("\n§§§ Sie haben vergessen eine Zahl einzugeben. " 
+								+ "Es wird die NICE Defautl-Zahl " + zahl + " initialisiert §§§");
+								
+		} catch (InputMismatchException i) {
+			
+			zahl = 4;
+			System.out.println("\n$$$ Das von Ihnen eingegeben Zeichen ist keine Zahl. Nur Zahlen sind erlaubt!. " 
+								+ "Es wird die FANTASTIC Defautl-Zahl " + zahl + " initialisiert $$$");
+													
+		} catch (IllegalArgumentException ie) {
+			
+			zahl = 10;
+			System.out.println("\n" + ie.getMessage() + "*** Es wird die Defautl-Zahl " + zahl + " initialisiert ***");
+		}
+		
+		try {
+			int ergebnis = twentyBy(zahl);
+			System.out.println("\n20 dividiert durch " + zahl + " gleich " + ergebnis);
+		} catch (ArithmeticException e) {
+			System.out.println("\nßßß Keine Division durch Null möglich ßßß");
+		}
+	}
+
+/** Methode zum Prüfen ob die Zahl größer als 10 ist. 
+  * throws eine Exception, falls dies der Fall ist.
+  * @param zahl ist zu prüfende Zahl
+*/
+	public static void istKleinerAlsZehn (int zahl) {
+		if (zahl <= 10) return ;	
+		throw new IllegalArgumentException("### Zahl ist groesser als 10 ###\n");
+	} 
+	
+/** Methode aus Aufgabe 13. Es wird die Zahl durch 20 dividiert 
+  * @param zahl ist zu dividierende Zahl
+*/
+	public static int twentyBy (int zahl) {
+		return 20 / zahl;
+	} 
+
+}
---
+++ blatt5/app/src/test/java/Zahlleser/ZahlLeserTest.java
@@ -0,0 +1,17 @@
+package Zahlleser;
+
+import org . junit . Test ;
+import static org . junit . Assert .*;
+
+public class ZahlLeserTest {
+	
+	@Test public void twentyByTest() {
+		assertEquals(3, ZahlLeser.twentyBy(6));
+		assertEquals(5, ZahlLeser.twentyBy(4));
+		assertEquals(10, ZahlLeser.twentyBy(2));
+	}
+}
+
+/** Der notwendige Befehl, um alle Test auszuführen ist:
+  * ./gradlew test
+*/
